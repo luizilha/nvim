2ccc4a61bdb6e5a0a5b91900fc59c8944f56ca02
luiz ilha <luizilham@gmail.com>
2024-02-07 14:33:56 -0300

    change telescope keybinds

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,7 +5,8 @@ return {
     config = function()
       local builtin = require("telescope.builtin")
       vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      vim.keymap.set('n', '<C-f>', builtin.live_grep, {}) -- need install ripgrep to use this
+      vim.keymap.set('n', '<C-b>', builtin.buffers, {})
     end
   },
   {
