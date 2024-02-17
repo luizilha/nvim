81da3601df60333fe53ac13bebbfaad3b3c32c18
luiz ilha <luizilham@gmail.com>
2024-02-17 11:26:33 -0300

    add git branch graph plugin

--- lua/plugins/git-tips.lua
+++ lua/plugins/git-tips.lua
@@ -1,13 +1,21 @@
 return {
-  {
-    "tpope/vim-fugitive",
-  },
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup()
-      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-      vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
-    end,
-  },
+	{
+		"tpope/vim-fugitive",
+	},
+	{
+		"rbong/vim-flog",
+		lazy = true,
+		cmd = { "Flog", "Flogsplit", "Floggit" },
+		dependencies = {
+			"tpope/vim-fugitive",
+		},
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+			vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+		end,
+	},
 }
